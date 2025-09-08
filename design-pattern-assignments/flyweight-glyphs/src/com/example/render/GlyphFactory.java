97346f2ddb56a2aba441cea2db4dd42d5581f51b
skushagra <kushagra.rigel@gmail.com>
2025-09-08 13:45:38 +0530

    thread safety

--- design-pattern-assignments/flyweight-glyphs/src/com/example/render/GlyphFactory.java
+++ design-pattern-assignments/flyweight-glyphs/src/com/example/render/GlyphFactory.java
@@ -1,33 +1,26 @@
 package com.example.render;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class GlyphFactory {
 
     Map<String, FontStyle> fontMap;
 
     GlyphFactory() {
-        this.fontMap = new HashMap<>();
+        this.fontMap = new ConcurrentHashMap<>();
     }
 
     public Glyph create(char c, String font, int size, boolean isBold) {
 
         String key = font + "-" + size + "-" + (isBold ? "bold" : "notBold");
 
-        if (this.fontMap.containsKey(key)) {
-            FontStyle fs = this.fontMap.get(key);
-            return new Glyph(c, fs);
-        }
+        FontStyle fs = this.fontMap.computeIfAbsent(key, _ -> {
+            System.out.println("Create fontstyle");
+            return new FontStyle(font, size, isBold);
+        });
 
-        FontStyle fs = new FontStyle(font, size, isBold);
-        System.out.println("Create fontstyle");
-
-        this.fontMap.put(key, fs);
-
-        Glyph g = new Glyph(c, fs);
-
-        return g;
+        return new Glyph(c, fs);
     }
 
 }
